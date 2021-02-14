d21bae88b5414de120162beb2870185e3d36e4e1
kaoutar1987 <kaoutarhabach@gmail.com>
2021-02-14 22:10:44 +0100

    install express

---
+++ main.js
@@ -0,0 +1,19 @@
+//Imports 
+let express =  require('express');
+
+//Instantiate server 
+let server = express();
+
+//Configure routes 
+server.get('/',function(req, res){
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send('<h1>Bonjour sur mon super server</h1>');
+
+});
+//Launch server 
+server.listen(8080,function(){
+    console.log('Server en écoute :')
+
+});
+
+//
